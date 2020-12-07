a9a007e0790c90bd846655dfa9c317d61efbb71f
teoitinga <teo.itinga@gmail.com>
2020-12-06 23:19:52 -0300

    Correção dos testes

--- src/main/java/com/jp/dark/services/PricesItemService.java
+++ src/main/java/com/jp/dark/services/PricesItemService.java
@@ -1,6 +1,8 @@
 package com.jp.dark.services;
 
 import com.jp.dark.models.entities.PricesItem;
+import org.springframework.stereotype.Service;
+
 
 public interface PricesItemService {
     PricesItem findById(String cod);
---
+++ src/main/java/com/jp/dark/services/impls/PricesItemServiceImpl.java
@@ -0,0 +1,16 @@
+package com.jp.dark.services.impls;
+
+import com.jp.dark.models.entities.PricesItem;
+import com.jp.dark.models.repository.PricesItemRepository;
+import com.jp.dark.services.PricesItemService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PricesItemServiceImpl implements PricesItemService {
+    private PricesItemRepository repository;
+
+    @Override
+    public PricesItem findById(String cod) {
+        return this.repository.findById(Integer.valueOf(cod)).get();
+    }
+}
